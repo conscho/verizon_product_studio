74e6e9f18552ee00d2688edd4c8f672554c01a5c
Constantin Scholl <constantin@scholl.im>
2016-11-17 16:56:16 -0500

    moving one dot

--- map.js
+++ map.js
@@ -8,93 +8,93 @@ function initMap() {
             {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
             {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
             {
-              featureType: 'administrative.locality',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
+                featureType: 'administrative.locality',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#d59563'}]
             },
             {
-              featureType: 'poi',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
+                featureType: 'poi',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#d59563'}]
             },
             {
-              featureType: 'poi.park',
-              elementType: 'geometry',
-              stylers: [{color: '#263c3f'}]
+                featureType: 'poi.park',
+                elementType: 'geometry',
+                stylers: [{color: '#263c3f'}]
             },
             {
-              featureType: 'poi.park',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#6b9a76'}]
+                featureType: 'poi.park',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#6b9a76'}]
             },
             {
-              featureType: 'road',
-              elementType: 'geometry',
-              stylers: [{color: '#38414e'}]
+                featureType: 'road',
+                elementType: 'geometry',
+                stylers: [{color: '#38414e'}]
             },
             {
-              featureType: 'road',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#212a37'}]
+                featureType: 'road',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#212a37'}]
             },
             {
-              featureType: 'road',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#9ca5b3'}]
+                featureType: 'road',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#9ca5b3'}]
             },
             {
-              featureType: 'road.highway',
-              elementType: 'geometry',
-              stylers: [{color: '#746855'}]
+                featureType: 'road.highway',
+                elementType: 'geometry',
+                stylers: [{color: '#746855'}]
             },
             {
-              featureType: 'road.highway',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#1f2835'}]
+                featureType: 'road.highway',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#1f2835'}]
             },
             {
-              featureType: 'road.highway',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#f3d19c'}]
+                featureType: 'road.highway',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#f3d19c'}]
             },
             {
-              featureType: 'transit',
-              elementType: 'geometry',
-              stylers: [{color: '#2f3948'}]
+                featureType: 'transit',
+                elementType: 'geometry',
+                stylers: [{color: '#2f3948'}]
             },
             {
-              featureType: 'transit.station',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
+                featureType: 'transit.station',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#d59563'}]
             },
             {
-              featureType: 'water',
-              elementType: 'geometry',
-              stylers: [{color: '#17263c'}]
+                featureType: 'water',
+                elementType: 'geometry',
+                stylers: [{color: '#17263c'}]
             },
             {
-              featureType: 'water',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#515c6d'}]
+                featureType: 'water',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#515c6d'}]
             },
             {
-              featureType: 'water',
-              elementType: 'labels.text.stroke',
-              stylers: [{color: '#17263c'}]
-          },
-          {
-            featureType: 'poi',
-            stylers: [{visibility: 'off'}]
-          },
-          {
-            featureType: 'transit',
-            stylers: [{visibility: 'off'}]
-        },
-        {
-          featureType: 'road',
-          elementType: 'labels.icon',
-          stylers: [{visibility: 'off'}]
-        },
+                featureType: 'water',
+                elementType: 'labels.text.stroke',
+                stylers: [{color: '#17263c'}]
+            },
+            {
+                featureType: 'poi',
+                stylers: [{visibility: 'off'}]
+            },
+            {
+                featureType: 'transit',
+                stylers: [{visibility: 'off'}]
+            },
+            {
+                featureType: 'road',
+                elementType: 'labels.icon',
+                stylers: [{visibility: 'off'}]
+            },
         ]
     });
     // [Chris] Plot drawing layer: https://developers.google.com/maps/documentation/javascript/examples/drawing-tools
@@ -105,7 +105,7 @@ function initMap() {
         circleOptions: {
             fillColor: '#ffff00',
             fillOpacity: 0.3,
-            strokeWeight: 1,
+            strokeWeight: 0.1,
             clickable: false,
             editable: false,
             zIndex: 1
@@ -146,20 +146,39 @@ function initMap() {
         }
     }
 
-    //[Chris] Info window: https://developers.google.com/maps/documentation/javascript/infowindows
-    var contentString = 'This is a Info window';
+    // Moving marker demo
+    function createMarker(latlng, label, text) {
+        var contentString = '<b>'+label+'</b><br>'+text;
+        var marker = new google.maps.Marker({
+            position: latlng,
+            map: map,
+            title: label,
+            zIndex: Math.round(latlng.lat()*-100000)<<5
+        });
+        marker.myname = label;
 
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
+        infowindow = new google.maps.InfoWindow({});
 
-    var marker = new google.maps.Marker({
-        position: {lat: 40.741, lng: -74.005},
-        map: map,
-        title: 'Chelsea '
-    });
+        marker.addListener('click', function() {
+            infowindow.setContent(contentString);
+            infowindow.open(map, marker);
+        });
+        return marker;
+    }
 
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
-    });
+    latlng = new google.maps.LatLng(40.744, -74.009)
+    marker = createMarker(latlng, 'test', 'green');
+
+
+    function moveMarkerBy(deltaX = 0, deltaY = 0) {
+        latlng = new google.maps.LatLng(marker.getPosition().lat() + deltaX, marker.getPosition().lng() + deltaY);
+        marker.setPosition(latlng);
+    }
+
+    for (var i=0;i<=100;i++) {
+        (function(ind) {
+            setTimeout(function(){moveMarkerBy(0.0001);}, 100 * ind);
+        })(i);
+    }
+
 }
