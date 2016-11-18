a976378f98941821617aa6e134b9fcb4f40e6bd1
Constantin Scholl <constantin@scholl.im>
2016-11-17 19:18:21 -0500

    move all dots

--- map.js
+++ map.js
@@ -1,4 +1,6 @@
 function initMap() {
+    var markers;
+
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 40.740914, lng: -74.0043697},
         zoom: 15,
@@ -97,11 +99,6 @@ function initMap() {
             },
         ]
     });
-    
-    // Run individual features of map
-    show_clustering()
-    // show_heatmap()
-    // show_infowindow()
 
     // [Chris] Plot drawing layer: https://developers.google.com/maps/documentation/javascript/examples/drawing-tools
     var drawingManager = new google.maps.drawing.DrawingManager({
@@ -153,15 +150,11 @@ function initMap() {
     }
 
     // Moving marker demo
-    function createMarker(latlng, label, text) {
-        var contentString = '<b>'+label+'</b><br>'+text;
+    function createMarker(input) {
+        var contentString = '<b>'+input.label+'</b><br>'+input.text;
         var marker = new google.maps.Marker({
-            position: latlng,
-            map: map,
-            title: label,
-            zIndex: Math.round(latlng.lat()*-100000)<<5
+            position: {lat: input.lat, lng: input.lng}
         });
-        marker.myname = label;
 
         infowindow = new google.maps.InfoWindow({});
 
@@ -172,107 +165,112 @@ function initMap() {
         return marker;
     }
 
-    latlng = new google.maps.LatLng(40.744, -74.009)
-    marker = createMarker(latlng, 'test', 'green');
-
-
-    function moveMarkerBy(deltaX = 0, deltaY = 0) {
+    function moveMarkerBy(marker, deltaX = 0, deltaY = 0) {
         latlng = new google.maps.LatLng(marker.getPosition().lat() + deltaX, marker.getPosition().lng() + deltaY);
         marker.setPosition(latlng);
     }
 
-    for (var i=0;i<=100;i++) {
-        (function(ind) {
-            setTimeout(function(){moveMarkerBy(0.0001);}, 100 * ind);
-        })(i);
+    function moveMarkers(markers, clustering) {
+        for (var i = 0; i < markers.length; i++) {
+            moveMarkerBy(markers[i], 0, 0.00002);
+        }
+        //var clusteringNew = update_clustering(markers);
+        clustering.clearMarkers();
+        //clustering = clusteringNew;
+        clustering.addMarkers(markers);
     }
 
     //[Chris] Marker clustering
-    function show_clustering() {
-        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-        var markers = locations.map(function(location, i) {
-            return new google.maps.Marker({
-              position: location,
-              label: labels[i % labels.length]
-          });
-        });
-
+    function update_clustering(markers) {
         var markerCluster = new MarkerClusterer(map, markers, {imagePath: 'images/m'});
+        return markerCluster;
+    }
 
+    // Initialize markers
+    var markers = locations.map(createMarker);
+
+    // Run individual features of map
+    var clustering = update_clustering(markers);
+    // show_heatmap()
+    // show_infowindow()
+
+
+    // Move all markers
+    for (var i=0;i<=100;i++) {
+        (function(ind) {
+            setTimeout(function(){moveMarkers(markers, clustering);}, 500 * ind);
+        })(i);
     }
 
 }
 
 var locations = [
-{lat: 40.741161, lng: -74.000816},
-{lat: 40.741234, lng: -74.001557},
-{lat: 40.741754, lng: -74.004228},
-{lat: 40.740405, lng: -74.003273},
-{lat: 40.740665, lng: -73.999465},
-{lat: 40.739706, lng: -73.997941},
-{lat: 40.739519, lng: -74.000623},
-{lat: 40.740559, lng: -74.003112},
-{lat: 40.74199, lng: -74.001696},
-{lat: 40.740575, lng: -74.004464},
-{lat: 40.740299, lng: -74.003134},
-{lat: 40.739535, lng: -74.00058},
-{lat: 40.741307, lng: -74.000967},
-{lat: 40.743648, lng: -74.001417},
-{lat: 40.743876, lng: -74.004207},
-{lat: 40.742689, lng: -74.006824},
-{lat: 40.741778, lng: -74.00661},
-{lat: 40.741242, lng: -74.006717},
-{lat: 40.740234, lng: -74.006352},
-{lat: 40.7396, lng: -74.006546},
-{lat: 40.739486, lng: -74.004121},
-{lat: 40.740055, lng: -74.004571},
-{lat: 40.740348, lng: -74.003735},
-{lat: 40.740787, lng: -74.003048},
-{lat: 40.741973, lng: -74.002447},
-{lat: 40.743225, lng: -74.002812},
-{lat: 40.744331, lng: -74.003842},
-{lat: 40.743518, lng: -74.005215},
-{lat: 40.74303, lng: -74.004979},
-{lat: 40.74251, lng: -74.00691},
-{lat: 40.740153, lng: -74.004185},
-{lat: 40.739958, lng: -74.001825},
-{lat: 40.74012, lng: -74.000881},
-{lat: 40.740559, lng: -73.999357},
-{lat: 40.741535, lng: -73.999121},
-{lat: 40.742624, lng: -73.999035},
-{lat: 40.742721, lng: -74.000366},
-{lat: 40.743177, lng: -74.002233},
-{lat: 40.743827, lng: -74.00249},
-{lat: 40.742364, lng: -74.001825},
-{lat: 40.739226, lng: -74.00146},
-{lat: 40.739958, lng: -74.002748},
-{lat: 40.737226, lng: -74.002039},
-{lat: 40.739779, lng: -74.00131},
-{lat: 40.739811, lng: -74.000022},
-{lat: 40.742006, lng: -74.002705},
-{lat: 40.739356, lng: -74.00279},
-{lat: 40.739925, lng: -74.002361},
-{lat: 40.740982, lng: -74.001696},
-{lat: 40.741778, lng: -74.000173},
-{lat: 40.740657, lng: -74.000752},
-{lat: 40.739421, lng: -74.002125},
-{lat: 40.73921, lng: -74.003048},
-{lat: 40.739958, lng: -74.002748},
-{lat: 40.740201, lng: -74.001975},
-{lat: 40.739632, lng: -74.002426},
-{lat: 40.739681, lng: -74.002855},
-{lat: 40.739892, lng: -74.002275},
-{lat: 40.738072, lng: -73.998263},
-{lat: 40.737535, lng: -73.998671},
-{lat: 40.737568, lng: -73.998156},
-{lat: 40.73799, lng: -73.997898},
-{lat: 40.738364, lng: -73.998349},
-{lat: 40.740852, lng: -73.998692},
-{lat: 40.741502, lng: -73.998263},
-{lat: 40.741518, lng: -73.997619},
-{lat: 40.740998, lng: -73.997405},
-{lat: 40.740039, lng: -74.004142},
-
+    {lat: 40.741161, lng: -74.000816 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.741234, lng: -74.001557 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.741754, lng: -74.004228 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.740405, lng: -74.003273 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.740665, lng: -73.999465 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.739706, lng: -73.997941 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.739519, lng: -74.000623 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.740559, lng: -74.003112 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.74199, lng: -74.001696 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.740575, lng: -74.004464 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.740299, lng: -74.003134 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.739535, lng: -74.00058 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.741307, lng: -74.000967 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.743648, lng: -74.001417 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.743876, lng: -74.004207 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.742689, lng: -74.006824 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.741778, lng: -74.00661 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.741242, lng: -74.006717 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.740234, lng: -74.006352 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.7396, lng: -74.006546 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.739486, lng: -74.004121 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.740055, lng: -74.004571 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.740348, lng: -74.003735 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.740787, lng: -74.003048 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.741973, lng: -74.002447 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.743225, lng: -74.002812 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.744331, lng: -74.003842 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.743518, lng: -74.005215 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.74303, lng: -74.004979 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.74251, lng: -74.00691 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.740153, lng: -74.004185 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.739958, lng: -74.001825 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.74012, lng: -74.000881 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.740559, lng: -73.999357 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.741535, lng: -73.999121 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.742624, lng: -73.999035 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.742721, lng: -74.000366 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.743177, lng: -74.002233 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.743827, lng: -74.00249 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.742364, lng: -74.001825 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.739226, lng: -74.00146 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.739958, lng: -74.002748 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.737226, lng: -74.002039 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.739779, lng: -74.00131 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.739811, lng: -74.000022 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.742006, lng: -74.002705 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.739356, lng: -74.00279 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.739925, lng: -74.002361 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.740982, lng: -74.001696 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.741778, lng: -74.000173 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.740657, lng: -74.000752 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.739421, lng: -74.002125 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.73921, lng: -74.003048 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.739958, lng: -74.002748 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.740201, lng: -74.001975 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.739632, lng: -74.002426 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.739681, lng: -74.002855 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.739892, lng: -74.002275 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.738072, lng: -73.998263 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.737535, lng: -73.998671 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.737568, lng: -73.998156 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.73799, lng: -73.997898 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.738364, lng: -73.998349 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.740852, lng: -73.998692 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.741502, lng: -73.998263 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.741518, lng: -73.997619 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.740998, lng: -73.997405 , label: 'Test Label', text: 'This is a test'},
+    {lat: 40.740039, lng: -74.004142 , label: 'Test Label', text: 'This is a test'},
 ]
-
